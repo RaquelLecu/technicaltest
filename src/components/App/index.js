f91761fe933e4346be12d1ca735a232e7a86a572
RaquelLecu <108267164+RaquelLecu@users.noreply.github.com>
2023-01-03 15:31:36 +0100

    update part1

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,14 @@
+import Part1 from "../Part1"
+import { data } from "../../resources/data"
 
 function App () {
     return (
         <div className='App'>
           <h1>Technical Test</h1>
+          <h2>Part 1</h2>
+          <Part1 data={ data }/>
         </div>
     )
 }
+
 export default App
--- src/resources/data.js
+++ src/resources/data.js
@@ -1,4 +1,4 @@
-const data = [
+export const data = [
     {
       gender: "male",
       name: {
