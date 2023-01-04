077de3beed204b72e7ef4f2193c9859b8c46bf92
RaquelLecu <108267164+RaquelLecu@users.noreply.github.com>
2023-01-04 11:16:37 +0100

    update part2

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,26 @@
-import Part1 from "../Part1"
+import ListPersons from "../ListPersons"
 import { data } from "../../resources/data"
+import { useState } from "react"
 
 function App () {
+  const [numPersons, setNumPersons] = useState(0)
+  const [persons, setPersons] = useState([]);
+  const addPersons= () => {
+    setPersons([...persons, data[numPersons]])
+    if(numPersons<data.length) setNumPersons(numPersons + 1)
+    else{
+      setNumPersons(0)
+      setPersons([])
+    } 
+  }
     return (
         <div className='App'>
           <h1>Technical Test</h1>
           <h2>Part 1</h2>
-          <Part1 data={ data }/>
+          <ListPersons data={ data }/>
+          <h2>Part 2</h2>
+          <button onClick= {addPersons}>New Person</button>
+          <ListPersons data={ persons }/>
         </div>
     )
 }
